1d72d361f577b7d2d7c28d922d2443843f288048
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-02 21:17:45 -0700

    Moving css file back to resources/css folder

--- client/src/components/ArticlePage/ArticlePage.js
+++ client/src/components/ArticlePage/ArticlePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
-import './ArticlePage.css';
+import '../../resources/css/ArticlePage';
 import {SearchBar} from '../SearchBar';
 import {Footer} from '../Footer';
 import {Link} from 'react-router-dom';
