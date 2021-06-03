8a777a56b2dd6fb2c2f3b821bbf32239fb4d2182
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-02 21:35:13 -0700

    removing getDate function and replacing with moment.js

--- client/src/components/HomePage/ArticlePreviews.js
+++ client/src/components/HomePage/ArticlePreviews.js
@@ -1,26 +1,10 @@
 import React from 'react';
+import moment from 'moment';
 import '../../resources/css/ArticlePreviews.css';
 import { Link } from 'react-router-dom';
 
 export class ArticlePreviews extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.getDate = this.getDate.bind(this);
-    }
-
-    getDate(){
-        const date = new Date(this.props.article.date);
-        const mm = String(date.getMonth() + 1).padStart(2, '0')
-        const dd = String(date.getDate() + 1).padStart(2, '0');
-        const yyyy = date.getFullYear();
-        const today = mm + "-" + dd + "-" + yyyy;
-
-        return today;
-    }
-
     render() {
-        // const DIR = "C:/Projects/ConceptualCoder/api/public/images/";
         const id = this.props.article._id;
         return(
             <div className="ArticlePreviews">
@@ -28,7 +12,7 @@ export class ArticlePreviews extends React.Component {
                     <div id="img-div">
                         <img className="article-img" src={this.props.article.img} alt={this.props.title} />
                     </div>
-                    <p id="preview-date">{this.getDate()}</p>
+                    <p id="preview-date">{moment(new Date(this.props.article.date)).format('MM/DD/YYYY')}</p>
                     <div id="preview-text">
                         <h3 id="preview-title">{this.props.article.title}</h3>
                         <p id="preview-summary">{this.props.article.summary}</p>
