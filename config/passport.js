4892178f500e7f89274336408315c5c4411e7853
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-18 12:15:25 -0700

    removing console.log statements

--- config/passport.js
+++ config/passport.js
@@ -1,20 +1,14 @@
-const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
 const Users = require('../models/Users');
 
 passport.serializeUser((user, done) => {
-    console.log('Serialized user');
-
     done(null, user._id);
 })
 
 passport.deserializeUser((id, done) => {
-    console.log("Deserializing user...");
     Users.findById(id, (err, user) => {
-            console.log("Deserialized user ");
-
             done(null, user);
         }
     )
