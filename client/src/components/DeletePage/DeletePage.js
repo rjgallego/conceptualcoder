9e16aa871f678d4ebdbd395770b87b14c36f04a6
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-18 13:22:01 -0700

    removing hard-coded DEV and PROD links and just using endpoints for back-end communication

--- client/src/components/DeletePage/DeletePage.js
+++ client/src/components/DeletePage/DeletePage.js
@@ -5,15 +5,15 @@ export class DeletePage extends React.Component {
   constructor(props){
       super(props);
       this.state = {
-        id: this.props.match.params.id,
-        link: process.env.NODE_ENV === 'production' ? 'https://conceptualcoder.herokuapp.com' : 'http://localhost:4001'
+        id: this.props.match.params.id
+        // link: process.env.NODE_ENV === 'production' ? 'https://conceptualcoder.herokuapp.com' : 'http://localhost:4001'
       }
 
       this.deleteArticle = this.deleteArticle.bind(this);
   } 
 
   deleteArticle() {
-    fetch(`${this.state.link}/delete/${this.state.id}`);
+    fetch(`/delete/${this.state.id}`);
   }
 
   componentDidMount() {
