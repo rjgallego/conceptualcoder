78945bd28ad0d29230862205bb44b99f0e46c9cf
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-18 13:23:40 -0700

    removing hard-coded DEV and PROD links and just using endpoints for back-end communication

--- client/src/components/Helpers.js
+++ client/src/components/Helpers.js
@@ -1,14 +1,11 @@
 export function checkLogin(setState){
-    const destUrl = process.env.NODE_ENV === 'production' ? 'https://conceptualcoder.herokuapp.com' : 'http://localhost:4001';
-    const srcUrl = process.env.NODE_ENV === 'production' ? 'https://conceptualcoder.herokuapp.com' : 'http://localhost:3000'
-    fetch(`${destUrl}/users/checkLogin`, {
+    fetch('/users/checkLogin', {
         method: 'get',
-           credentials: "include",
-           headers: {
+        credentials: "include",
+        headers: {
                'Content-Type': 'application/json',
-               'Accept': 'application/json',
-               'Access-Control-Allow-Origin': srcUrl
-           }
+               'Accept': 'application/json'
+        }
      })
      .then(response => response.json())
      .then(jsonResponse => setState(jsonResponse.loggedIn));
