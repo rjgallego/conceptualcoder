62d064582708a837ff48b1deba7ca745bf4b2293
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-06-18 13:26:32 -0700

    removing CORS configuration that is no longer needed.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors');
+// var cors = require('cors');
 var mongoose = require('mongoose');
 var session = require('express-session');
 var bodyParser = require('body-parser');
@@ -18,7 +18,7 @@ var userRouter = require('./routes/api/users');
 var editRouter = require('./routes/edit');
 
 var app = express();
-let corsOptions = {};
+// let corsOptions = {};
 
 const PORT = process.env.PORT || 4001;
 
@@ -43,21 +43,25 @@ app.use(cookieParser("coderSession"));
 
 if(process.env.ENVIRONMENT === 'development'){
   app.use(logger('dev'));
-  app.use(express.static(path.join(__dirname, 'public')));
+  // app.use(express.static(path.join(__dirname, 'public')));
 
-  corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true
-  };
-} else if(process.env.ENVIRONMENT === 'production'){
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-  corsOptions = {
-    origin: 'https://conceptualcoder.herokuapp.com/',
-    credentials: true
-  };
+  // corsOptions = {
+  //   origin: 'http://localhost:3000',
+  //   credentials: true
+  // };
 } 
+// else if(process.env.ENVIRONMENT === 'production'){
+//   app.use(express.static(path.join(__dirname, 'client', 'build')));
+//   corsOptions = {
+//     origin: 'https://conceptualcoder.herokuapp.com/',
+//     credentials: true
+//   };
+// } 
 
-app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+
+//app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({ 
@@ -76,18 +80,14 @@ app.use('/delete', deleteRouter);
 app.use('/users', userRouter);
 app.use('/edit', editRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function(err, req, res) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
